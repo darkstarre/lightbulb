95ab813585e55650f1c0e119f07c4d841a44ce22
Megan Nilles <darkstarre@hotmail.com>
2016-04-22 10:39:46 -0500

    edit/delete controllers and models

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,4 +6,19 @@ class IdeasController < ApplicationController
   def new
 
   end
+
+  def update
+    idea = Idea.find_by(params[:id])
+    idea.update(params[:idea].symbolize_keys)
+  end
+
+  def edit
+    @idea = Idea.find_by(params[:id])
+  end
+
+  def destroy
+    Idea.destroy(params[:id])
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
    root "ideas#index"
    get "ideas/new", to: "ideas#new", as: "new_idea"
+   get "ideas/edit/:id", to: "ideas#edit", as: "edit_idea"
+   patch "ideas/:id", to: "ideas#update", as: "update_idea"
+   delete "ideas/:id", to: "ideas#destroy", as: "destroy_idea"
 end
+
+
