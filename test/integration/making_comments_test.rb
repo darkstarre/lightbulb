a914e34dc834909be315f2640f3e6d73eca442e1
Dongmin Lee <medi8600@gmail.com>
2016-04-22 17:23:40 -0500

    Comments are Green

--- test/integration/making_comments_test.rb
+++ test/integration/making_comments_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class MakingIdeasTest < ActionDispatch::IntegrationTest
-  def test_comments_can_run_the_gammet_of_crud
+  def test_comments_can_be_made_and_deleted
     my_idea = Idea.create body: "Comment about me"
     page.visit idea_path(my_idea)
 
@@ -11,8 +11,8 @@ class MakingIdeasTest < ActionDispatch::IntegrationTest
     page.click_button("Create Comment")
     assert page.has_content?('This is a great idea!')
 
-    assert page.has_button?("Delete this Comment")
-    page.click_button?("Delete this Comment")
+    assert page.has_link?("Delete Comment")
+    page.click_link("Delete Comment")
     refute page.has_content?('This is a great idea!')
   end
 end
