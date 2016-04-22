2fbd254d40c38f917bf3b947fb9d1157222fb46b
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-04-22 14:15:08 -0500

    Ideas are now working

--- test/integration/making_ideas_test.rb
+++ test/integration/making_ideas_test.rb
@@ -5,8 +5,8 @@ class MakingIdeasTest < ActionDispatch::IntegrationTest
   def test_ideas_can_run_the_gammet_of_crud
     page.visit ideas_path
 
-    assert page.has_content?("Light up the world with your idea")
-    page.click_link ("Light up the world with your idea")
+    assert page.has_content?("Light up the world")
+    page.click_link ("Light up the world")
 
     assert page.has_button?("Create Idea")
 
@@ -16,17 +16,17 @@ class MakingIdeasTest < ActionDispatch::IntegrationTest
     page.click_button('Create Idea')
     assert page.has_content?('blah blah blog')
 
-    page.first('.i').click_link('1')
+    page.click_link('Idea:')
     assert page.has_content?("Edit")
     page.click_link('Edit')
-    assert page.has_content?("Update Idea")
+    assert page.has_button?("Update Idea")
     fill_in('idea_body', :with => 'blah blah edited blog')
     page.click_button('Update Idea')
 
     assert page.has_content?('blah blah edited blog')
-    page.first('.i').click_link('1')
+    page.click_link('Idea:')
     assert page.has_content?("Delete")
-    assert page.click_first_link("Delete")
+    page.click_link("Delete")
 
     refute page.has_content?('blah blah edited blog')
   end
