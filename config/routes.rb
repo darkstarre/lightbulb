70b2329c3c62c424eeb14c1f5c5b5413b89c42a0
Megan Nilles <darkstarre@hotmail.com>
2016-04-22 11:51:27 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
    root "ideas#index"
-<<<<<<< HEAD
+
    get "/ideas", to: "ideas#index", as:"ideas"
    get "/ideas/new", to: "ideas#new", as: "new_idea"
    post "/ideas", to: "ideas#create"
    get "/ideas/:id", to: "ideas#show", as: "idea"
-=======
-   get "ideas/new", to: "ideas#new", as: "new_idea"
    get "ideas/edit/:id", to: "ideas#edit", as: "edit_idea"
    patch "ideas/:id", to: "ideas#update", as: "update_idea"
    delete "ideas/:id", to: "ideas#destroy", as: "destroy_idea"
->>>>>>> 1011a71eb44b3d7c03f822bf1aff031e796105a1
 end
 
 
